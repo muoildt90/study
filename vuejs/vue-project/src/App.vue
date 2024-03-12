025eec451633bd62fa85b4968c96f268a126716b
ThangLD <thangld@abilive.vn>
2024-03-12 14:51:01 +0700

    first add code

---
+++ vuejs/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ChildComp from './ChildComp.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      msg: 'slot1',
+      msg1: 'slot2'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp>{{ msg }}</ChildComp>
+</template>
